2fc1bf7574960666d2199ae7c1db6923de432b84
lucasvribeiro <lucasvribeiro14@gmail.com>
2024-08-06 21:02:11 -0300

    [FEAT] Create exame quizzes

---
+++ src/data/index.jsx
@@ -0,0 +1,12 @@
+export const AVAILABLE_QUIZZES = [
+  {
+    name: 'Covid-19 Quiz',
+    fileName: 'covid19Quiz.json',
+    path: 'covid-19'
+  },
+  {
+    name: 'Lifestyle Quiz',
+    fileName: 'lifestyleQuiz.json',
+    path: 'lifestyle'
+  }
+];
